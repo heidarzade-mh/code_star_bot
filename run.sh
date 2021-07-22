a63ab2bc9b9ce9e875b3a379f1ef3d9e6e6cd072
BijanEisapour <bijanprogrammer@gmail.com>
2021-07-23 00:17:31 +0430

    [UPDATE] run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-javac -cp lib\telegrambots-3.5-jar-with-dependencies.jar -d classes src\*.java -encoding utf8
+javac -cp lib/telegrambots-3.5-jar-with-dependencies.jar -d classes src/*.java -encoding utf8
 cd classes || exit
-java -cp "..\lib\telegrambots-3.5-jar-with-dependencies.jar;" App
+java -cp "../lib/telegrambots-3.5-jar-with-dependencies.jar;" App
 sleep 30
